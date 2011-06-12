6feb81667cbc4e9e11e8d55799bc863e6abd0eff
Levin Alexander <mail@levinalex.net>
2011-06-12 19:01:53 +0200

    remove explicit rubygems require from test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,10 @@
-require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'packet_io'
 require 'packet_io/test/mock_server'
 
