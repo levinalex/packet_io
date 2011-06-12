8d94b32c59cf185ee329df49433c50b35e764d06
Levin Alexander <mail@levinalex.net>
2011-06-12 19:06:56 +0200

    whitespace

--- test/test_io_listener.rb
+++ test/test_io_listener.rb
@@ -1,29 +1,29 @@
 require 'helper'
 
-  describe "a server" do
-    before do
-      @device, client_read, client_write = PacketIO::Test::MockServer.build
-      @server = PacketIO::IOListener.new(client_read, client_write)
-      @protocol = PacketIO::LineBasedProtocol.new(@server)
-    end
-
-    it "should exist" do
-      assert @server
-    end
-
-    it "should yield packets written to it" do
-      @packets = []
-      @protocol.on_data { |packet| @packets << packet }
-
-      @device.write("fo").wait.write("o\n").wait.write("bar\n").eof
-      @server.run!
-
-      assert_equal ["foo", "bar"], @packets
-    end
-
-    it "should send data" do
-      @protocol << "hello world"
-      data = @device.read_all
-      assert_equal "hello world\n", data
-    end
+describe "a server" do
+  before do
+    @device, client_read, client_write = PacketIO::Test::MockServer.build
+    @server = PacketIO::IOListener.new(client_read, client_write)
+    @protocol = PacketIO::LineBasedProtocol.new(@server)
   end
+
+  it "should exist" do
+    assert @server
+  end
+
+  it "should yield packets written to it" do
+    @packets = []
+    @protocol.on_data { |packet| @packets << packet }
+
+    @device.write("fo").wait.write("o\n").wait.write("bar\n").eof
+    @server.run!
+
+    assert_equal ["foo", "bar"], @packets
+  end
+
+  it "should send data" do
+    @protocol << "hello world"
+    data = @device.read_all
+    assert_equal "hello world\n", data
+  end
+end
